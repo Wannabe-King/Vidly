9db81ae4ae5867de329870e8bff6841db5f9293d
King <sidarsunita2@gmail.com>
2023-12-27 14:08:32 +0530

    handling unhandled errors and exceptions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('express-async-errors')
 const winston=require('winston')
+require('winston-mongodb')
 const express=require("express");
 const app=express();
 const genres=require("./routes/genre")
@@ -11,6 +12,24 @@ const auth=require('./routes/auth')
 const mongoose=require("mongoose")
 const config=require('config')
 const error=require('./middleware/error')
+
+// Code for catching unCaughtException
+
+process.on('uncaughtException',(ex)=>{
+    console.log('WE GOT AN UNCAUGHT EXCEPTION')
+    winston.error(ex.message,ex)
+    process.exit(1)
+})
+
+process.on('unhandledRejection',(ex)=>{
+    console.log('WE GOT AN UNHANDLED REJECTION')
+    winston.error(ex.message,ex)
+    process.exit(1)
+})
+
+winston.add(winston.transports.File,{filename:'logfile.log'})
+winston.add(winston.transports.MongoDB,{db:'mongodb://localhost:27017/vidly'})
+
 
 if(!config.get('jwtPrivateKey')) {
     console.log('FATAL ERROR : jwtPrivateKey is not defined') 
--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,8 @@
+const winston=require('winston')
+
 module.exports = function(err,req,res,next){
 
+    winston.error(err.message,err)
     //Internal Server Error
     res.status(500).send('Internal Server Error');
 }
